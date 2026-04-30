2af3df423bd870a7523b214c04f4839cae526fdd
suryatharun4248 <suryatharuntejagajula@gmail.com>
2026-04-30 14:07:50 +0530

    fix react scripts

--- college-platform/server/db.js
+++ college-platform/server/db.js
@@ -1,10 +1,22 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+const isProduction = process.env.DATABASE_URL;
+
+const pool = new Pool(
+  isProduction
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {
+        user: "postgres",
+        host: "localhost",
+        database: "college_db",
+        password: "admin1234",
+        port: 5432,
+      }
+);
 
 module.exports = pool;
